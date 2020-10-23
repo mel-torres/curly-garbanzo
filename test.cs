1536661b21ce2e3175cefcff5409ddaa668691eb
mannylozano <64612922+mannylozano@users.noreply.github.com>
2020-10-22 19:43:32 -0600

    Update and rename test to test.cs

---
+++ test.cs
@@ -0,0 +1,21 @@
+#include 
+using namespace std;
+
+int main () {
+   
+   int a,b,max;
+   
+   cout  a; 
+   cout  b; 
+   
+   //remove the terminating character
+   std::cin.ignore();
+ 
+   if( ab ) {max=a;}
+        else {max=b;}
+   
+   cout   
+   //Extracts characters from the stream
+   std::cin.get();
+   return 0;
+}
